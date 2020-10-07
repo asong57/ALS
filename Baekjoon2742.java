07ec742b8f3b578a888261438bfd584a1be04f43
asong57 <asong57@naver.com>
2020-10-07 17:18:25 +0900

    Baekjoon 2742 completed on 201007

---
+++ Baekjoon2742.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Baekjoon2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number=sc.nextInt();
+        for(int i=number; i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
