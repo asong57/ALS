9a37ff84328f3d97d53650f18b28c43ba684bc19
asong57 <asong57@naver.com>
2020-12-19 22:47:57 +0900

    20201219

---
+++ CodeUp1022.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CodeUp1022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(word);
+    }
+}
