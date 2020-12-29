b7d8a18b67f5091bc36b836569b269d9a93ec371
asong57 <asong57@naver.com>
2020-12-29 23:36:58 +0900

    20201229

---
+++ CodeUp/CodeUp1029.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CodeUp1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double line = sc.nextDouble();
+        System.out.println(String.format("%.11f",line));
+
+    }
+}
