bba5edc9f4257fc929a209b5078361a470cf6b4a
asong57 <asong57@naver.com>
2021-01-19 23:41:00 +0900

    20210119

--- CodeUp/CodeUp1064.java
+++ CodeUp/CodeUp1064.java
@@ -6,6 +6,6 @@ public class CodeUp1064 {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
-        System.out.println(num1>num2?num1:num2);
+        System.out.println((num1<num2?num1:num2)<num3?(num1<num2?num1:num2):num3);
     }
 }
