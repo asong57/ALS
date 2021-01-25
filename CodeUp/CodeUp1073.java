3121322485b4dcb09050b0ccc66734a80b3911be
asong57 <asong57@naver.com>
2021-01-26 00:48:24 +0900

    20210126

---
+++ CodeUp/CodeUp1073.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CodeUp1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int num = sc.nextInt();
+            if(num == 0){
+                break;
+            }
+            System.out.println(num);
+        }
+    }
+}
