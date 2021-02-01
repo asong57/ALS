d0a2ab7f7ae5632f1c89c7612364ac6bbb9b3311
asong57 <asong57@naver.com>
2021-02-01 21:43:47 +0900

    20210201

--- CodeUp1022.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class CodeUp1022 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine();
-        System.out.println(word);
-    }
-}
