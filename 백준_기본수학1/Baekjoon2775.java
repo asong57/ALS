41932c5b68948287ca6d3ef0db0c35e214f465d0
asong57 <asong57@naver.com>
2021-02-10 00:12:56 +0900

    20210209

--- 백준_기본수학1/Baekjoon2775.java
+++ 백준_기본수학1/Baekjoon2775.java
@@ -2,46 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
 public class Baekjoon2775 {
 
-    public static int[][] APT = new int[15][15];
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        make_APT();	// 아파트 만들기
-
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-            sb.append(APT[k][n]).append('\n');
-        }
-        System.out.println(sb);
-    }
-
-
-    public static void make_APT() {
-        // 아파트 생성
-
-        for (int i = 0; i < 15; i++) {
-            APT[i][1] = 1; // i층 1호
-            APT[0][i] = i; // 0층 i호
-        }
-
-        for (int i = 1; i < 15; i++) { // 1층부터 14층까지
-
-            for (int j = 2; j < 15; j++) { // 2호부터 14호까지
-                APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
-            }
+        int number = Integer.parseInt(br.readLine());
+        int k,n;
+        for(int i=0;i<number;i++){
+            k=Integer.parseInt(br.readLine());
+            n=Integer.parseInt(br.readLine());
+            System.out.println(peopleCount(k,n));
         }
     }
-
+    public static int peopleCount(int k, int n){
+        if(k==0){
+            return n;
+        }
+        if(n==0){
+            return 0;
+        }
+        return peopleCount(k,n-1)+peopleCount(k-1,n);
+    }
 }
