e0cbb0f51e61caae1ac7f93d7a1a66c2a872d881
asong57 <asong57@naver.com>
2021-03-28 17:13:03 +0900

    20210328

---
+++ 백준_동적계획법/Baekjoon1912.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for(int i=1;i<n;i++){
+            dp[i] = Math.max(dp[i-1]+arr[i],arr[i]);
+            max = Math.max(max,dp[i]);
+        }
+        System.out.print(max);
+    }
+}
