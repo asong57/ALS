277e5ea86581c573a767d18850cdcabe544202b4
asong57 <asong57@naver.com>
2021-05-28 15:43:43 +0900

    가장 큰 수와 가장 작은 수의 조합

---
+++ 프로그래머스/Level2/SavingBoat.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class SavingBoat {
+    //보트에 가장 무게가 많이 나가는 사람과 가장 적게 나가는 사람을 같이 보낸다는 논리
+    
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        for (int max = people.length - 1, min = 0; max >= min; max--) {
+           if(people[max] + people[min] <= limit){
+               min++;
+               answer++;
+           }else{
+               answer++;
+           }
+        }
+        return answer;
+    }
+}
