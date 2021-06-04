defd4099d6c7bb80fdc9bc28bf9dc3d8bb066a65
asong57 <1998thddk@gmail.com>
2021-06-04 21:17:38 +0900

    DP 활용 연습문제

---
+++ 프로그래머스/Level2/EatTheGround.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class EatTheGround {
+    static int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][1], Math.max(land[i - 1][0], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][0]));
+        }
+
+        return Math.max(land[land.length - 1][0], Math.max(land[land.length - 1][1], Math.max(land[land.length - 1][2], land[land.length - 1][3])));
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
+        System.out.println(solution(land));
+    }
+}
