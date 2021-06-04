e10d2c84d603cf93ded84a815cade9ce7eb48155
asong57 <1998thddk@gmail.com>
2021-06-04 19:52:21 +0900

    피보나치, dp

---
+++ 프로그래머스/Level2/Fibonacchi.java
@@ -0,0 +1,14 @@
+public class Fibonacchi {
+    public static int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[0] =0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = (dp[i-1]+dp[i-2]) % 1234567;
+        }
+        return dp[n];
+    }
+    public static void main(String[] args){
+        System.out.println(solution(5));
+    }
+}
