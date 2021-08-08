3b94e236de5b3faca410e500025412a7e4bd85ac
asong57 <1998thddk@gmail.com>
2021-08-08 22:33:59 +0900

    조합, HashSet, 정규식활용

---
+++ Econovation_Algorithm_Mentoring/기출문제/Kakao2019THREE.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
+public class Kakao2019THREE {
+    //불량 사용자
+    static HashSet<HashSet<String>> result;
+    static ArrayList<ArrayList<String>> bannedUserList;
+
+    public static int solution(String[] user_id, String[] banned_id) {
+        result = new HashSet<HashSet<String>>();
+        bannedUserList = new ArrayList<ArrayList<String>>();
+
+        for (String bannedId : banned_id) {
+            // 목록의 n번째 칸에 들갈 수 있는 제재 아이디들 구함
+            bannedUserList.add(getMatchesId(bannedId, user_id));
+        }
+
+        dfs(new HashSet<String>(), 0);
+
+        return result.size();
+    }
+
+    public static ArrayList<String> getMatchesId(String bannedId, String[] user_id) {
+        // 불량 사용자 아이디 정규 표현식으로 변환
+        // 정규 표현식에서 '.' = 임의의 문자열 1개
+        String pattern = bannedId.replace('*', '.');
+
+        ArrayList<String> valueList = new ArrayList<>();
+
+        for (String userId : user_id) {
+            boolean isMatch = Pattern.matches(pattern, userId);
+
+            // 정규 표현식 턴에 맞는 유저 아이디 담음
+            if (isMatch)
+                valueList.add(userId);
+        }
+
+        return valueList;
+    }
+
+    static void dfs(HashSet<String> add, int depth) {
+        if (depth == bannedUserList.size()) {
+            // 결과 Set에 저장
+            result.add(new HashSet<>(add));
+            return;
+        }
+
+        // depth번째에 들어갈 수 있는 아이디 목록에서 뽑음
+        for (String userId : bannedUserList.get(depth)) {
+            // 이미 목록에 들어가있으면 담지 않음
+            if (!add.contains(userId)) {
+                add.add(userId);
+                dfs(add, depth + 1);
+                add.remove(userId);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] userid = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+        String[] bannedId = {"fr*d*", "abc1**"};
+        System.out.println(solution(userid, bannedId));
+    }
+}
