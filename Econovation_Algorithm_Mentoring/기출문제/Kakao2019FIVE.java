ce51147dbe7a7fae588b8b1ed52f8cceead2409c
asong57 <1998thddk@gmail.com>
2021-08-10 19:47:20 +0900

    이분탐색으로 다시 해결

--- Econovation_Algorithm_Mentoring/기출문제/Kakao2019FIVE.java
+++ Econovation_Algorithm_Mentoring/기출문제/Kakao2019FIVE.java
@@ -1,8 +1,48 @@
 public class Kakao2019FIVE {
     //징검다리 건너기
-    //효율성 실패
+
+    static long end = Integer.MAX_VALUE;
+    static long mid = 0;
+    static int count = 1;
+    static long start = 1;
+    static long max = 0;
 
     public static int solution(int[] stones, int k) {
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            count = 1;
+            check(stones, k);
+        }
+
+        return (int) max + 1;
+    }
+
+    public static void check(int[] stones, int k) {
+        for (int i = 0; i < stones.length; i++) {
+            if (count > k) {
+                end = mid - 1;
+                return;
+            }
+
+            if ((stones[i] - mid) <= 0) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+        }
+        if (count > k) {
+            end = mid - 1;
+            return;
+        }
+
+        max = Math.max(max, mid);
+        start = mid + 1;
+    }
+
+    //효율성 실패 (첫번째 풀이)
+    public static int solutionFirst(int[] stones, int k) {
         int friend = 0;
         int jump = 1;
         while (jump <= k) {
