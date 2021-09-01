a3310dba674c8b2938d06d3b2d280ccc7b0d2e85
asong57 <1998thddk@gmail.com>
2021-09-01 14:07:56 +0900

    [백준-완전탐색] ThreeDots : 이분탐색을 곁들인 완전탐색 풀이 첨부

--- Econovation_Algorithm_Mentoring/1. 완전 탐색 (Brute-force Search)/Baekjoon13423_ThreeDots.java
+++ Econovation_Algorithm_Mentoring/1. 완전 탐색 (Brute-force Search)/Baekjoon13423_ThreeDots.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -29,6 +30,47 @@ public class Baekjoon13423_ThreeDots {
                     if (k == n) break;
 
                     if (a[k] - a[j] == ba) cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+
+//좋은 풀이 (이분탐색 곁들이기)
+    static int T,N;
+    static int arr[];
+    public static void main2(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        T=Integer.parseInt(br.readLine());
+
+        for(int tc=1;tc<=T;tc++)
+        {
+            N=Integer.parseInt(br.readLine());
+            arr=new int[N];
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            for(int i=0;i<N;i++)
+            {
+                arr[i]=Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            int cnt=0;
+            for(int i=1;i<N-1;i++)
+            {
+                int l=0;
+                int r=N-1;
+                while(l<r)
+                {
+                    int lDist=arr[i]-arr[l];
+                    int rDist=arr[r]-arr[i];
+                    if(lDist==rDist)
+                    {
+                        cnt++;
+                        l++;
+                    }
+                    else if(lDist<rDist)
+                        r--;
+                    else
+                        l++;
                 }
             }
             System.out.println(cnt);
