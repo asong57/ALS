a92695fd6dbe48af4cb5f4f5dfe2721b5582bcbb
asong57 <1998thddk@gmail.com>
2021-09-08 09:32:19 +0900

    [프로그래머스-카카오2020 공채] 괄호변환 : 문제 그대로 따라가기 구현

---
+++ Econovation_Algorithm_Mentoring/기출문제집/2020KakaoBlind/Kakao2020_BracketTransition.java
@@ -0,0 +1,67 @@
+public class Kakao2020_BracketTransition {
+    public String solution(String p) {
+        return makeCorrect(p);
+    }
+
+    String makeCorrect(String w) {
+        if (w.length() == 0) {
+            return "";
+        }
+
+        int cut = division(w);
+        String u = w.substring(0, cut);
+        String v = w.substring(cut, w.length());
+
+        if (isCorrect(u)) {
+            return u + makeCorrect(v);
+        } else {
+            String temp = "(" + makeCorrect(v) + ")";
+            u = u.substring(1, u.length() - 1);
+            u = reverse(u);
+            return temp + u;
+        }
+    }
+
+    int division(String w) {
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < w.length(); i++) {
+            if (w.charAt(i) == '(') {
+                open++;
+            } else {
+                close++;
+            }
+            if (open == close) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    boolean isCorrect(String w) {
+        int count = 0;
+        for (int i = 0; i < w.length(); i++) {
+            if (w.charAt(i) == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    String reverse(String w) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < w.length(); i++) {
+            if (w.charAt(i) == '(') {
+                sb.append(")");
+            } else {
+                sb.append("(");
+            }
+        }
+        return sb.toString();
+    }
+}
