db1353aad58307309b252e48528c0dbbda13acbe
asong57 <1998thddk@gmail.com>
2021-11-05 21:15:58 +0900

    [백준] 스위프트 - 두 수 더하기

---
+++ Algorithm_Swift/Algorithm_Swift/Baekjoon/Baekjoon1000.swift
@@ -0,0 +1,17 @@
+//
+//  Baekjoon1000.swift
+//  Algorithm_Swift
+//
+//  Created by asong on 2021/11/05.
+//
+
+import Foundation
+
+func Baekjoon1000(){
+    
+    let input = readLine()!
+    let inputArr = input.split(separator: " ")
+    let a = Int(inputArr[0])!
+    let b = Int(inputArr[1])!
+    print(a+b)
+}
