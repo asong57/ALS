a3d4331137b28f22a4183dd5c194fcbb15b51220
asong57 <1998thddk@gmail.com>
2021-11-07 16:02:51 +0900

    [백준] 스위프트 - 홍준 출력

---
+++ Algorithm_Swift/Algorithm_Swift/Baekjoon/Baekjoon14581.swift
@@ -0,0 +1,17 @@
+//
+//  Baekjoon14581.swift
+//  Algorithm_Swift
+//
+//  Created by asong on 2021/11/07.
+//
+
+import Foundation
+
+func Baekjoon14581(){
+    let id = readLine()!
+
+    var hongjun: String = ":fan::"+id+"::fan:"
+    print(":fan::fan::fan:")
+    print(hongjun)
+    print(":fan::fan::fan:")
+}
