d08980aba3e80b84c59a1c785c00b53ddf929c6f
asong57 <1998thddk@gmail.com>
2021-12-26 20:47:29 +0900

    [스위프트] 구현

---
+++ Algorithm_Swift/Algorithm_Swift/Baekjoon/Baekjoon2558.swift
@@ -0,0 +1,14 @@
+//
+//  Baekjoon2558.swift
+//  Algorithm_Swift
+//
+//  Created by asong on 2021/12/26.
+//
+
+import Foundation
+
+func BJ2558(){
+    let A = Int(readLine()!)!
+    let B = Int(readLine()!)!
+    print(A+B)
+}
