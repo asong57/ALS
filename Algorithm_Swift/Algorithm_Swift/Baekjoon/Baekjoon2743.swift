0f5cc5d2006067acd545005d3b1c505550eb1a2d
asong57 <1998thddk@gmail.com>
2022-01-05 23:00:38 +0900

    [백준] 스위프트 - 구현

---
+++ Algorithm_Swift/Algorithm_Swift/Baekjoon/Baekjoon2743.swift
@@ -0,0 +1,13 @@
+//
+//  Baekjoon2743.swift
+//  Algorithm_Swift
+//
+//  Created by asong on 2022/01/05.
+//
+
+import Foundation
+
+func BJ2743(){
+    let input: String = readLine()!
+    print(input.count)
+}
