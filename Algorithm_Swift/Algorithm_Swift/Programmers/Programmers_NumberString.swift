d8aa2fc6d37dcdfefebeb29911e204b4ffd8ace1
asong57 <1998thddk@gmail.com>
2022-04-25 09:44:05 +0900

    [프로그래머스] 스위프트 - replacingOccurrences

--- Algorithm_Swift/Algorithm_Swift/Programmers/Programmers_NumberString.swift
+++ Algorithm_Swift/Algorithm_Swift/Programmers/Programmers_NumberString.swift
@@ -18,3 +18,18 @@ func solution(_ s:String) -> Int {
  
     return Int(str)!
 }
+
+func solution_NumberString2(_ s:String) -> Int {
+    var s = s
+    s = s.replacingOccurrences(of: "zero", with: "0")
+    s = s.replacingOccurrences(of: "one", with: "1")
+    s = s.replacingOccurrences(of: "two", with: "2")
+    s = s.replacingOccurrences(of: "three", with: "3")
+    s = s.replacingOccurrences(of: "four", with: "4")
+    s = s.replacingOccurrences(of: "five", with: "5")
+    s = s.replacingOccurrences(of: "six", with: "6")
+    s = s.replacingOccurrences(of: "seven", with: "7")
+    s = s.replacingOccurrences(of: "eight", with: "8")
+    s = s.replacingOccurrences(of: "nine", with: "9")
+    return Int(s)!
+}
