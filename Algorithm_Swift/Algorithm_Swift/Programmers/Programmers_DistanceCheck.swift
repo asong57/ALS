b4c255b008c7fae2dad8094776453a6c1a62d2dc
asong57 <1998thddk@gmail.com>
2022-04-25 10:17:45 +0900

    [프로그래머스] 스위프트 - bfs 두번

--- Algorithm_Swift/Algorithm_Swift/Programmers/Programmers_DistanceCheck.swift
+++ Algorithm_Swift/Algorithm_Swift/Programmers/Programmers_DistanceCheck.swift
@@ -70,3 +70,55 @@ func checkDistance(arr: [[Character]])-> Int {
     
     return 1
 }
+
+func solution_DistanceCheck2(_ places:[[String]]) -> [Int] {
+    var arr: [[Character]] = []
+    var answer: [Int] = []
+    for i in 0..<places.count{
+        arr = Array(repeating: [], count: 5)
+        for j in 0..<places[i].count{
+            for c in places[i][j]{
+                arr[j].append(c)
+            }
+        }
+       answer.append(checkPlace(arr))
+    }
+    return answer
+}
+func checkPlace(_ arr: [[Character]]) -> Int{
+    let dx = [0,1,0,-1]
+    let dy = [1,0,-1,0]
+    var visit = Array(repeating: Array(repeating: false, count: 5), count: 5)
+    for i in 0..<5{
+        for j in 0..<5{
+            if arr[i][j] == "P"{
+                visit[i][j] = true
+                l: for k in 0..<4{
+                    let xx = i + dx[k]
+                    let yy = j + dy[k]
+                    if xx >= 0 && yy >= 0 && xx < 5 && yy < 5 && !visit[xx][yy]{
+                        if arr[xx][yy] == "X"{
+                            continue l
+                        }
+                        if arr[xx][yy] == "P"{
+                            return 0
+                        }
+                        n: for h in 0..<4{
+                            if h != k && h % 2 == k % 2{
+                                    continue n
+                            }
+                            let xh = xx + dx[h]
+                            let yh = yy + dy[h]
+                            if xh >= 0 && yh >= 0 && xh < 5 && yh < 5 && !visit[xh][yh]{
+                                if arr[xh][yh] == "P"{
+                                    return 0
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 1
+}
